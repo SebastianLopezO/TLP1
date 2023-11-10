05b6e7296344092e8b58f863eb0b607edcc9de9e
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2023-11-09 19:48:16 -0500

    main

--- Prueba/src/Main.java
+++ Prueba/src/Main.java
@@ -3,12 +3,19 @@ import javax.naming.NamingException;
 import Services.SaludoBeanRemote;
 
 public class Main {
-	public static void main(String[] args) throws NamingException {
-		SaludoBeanRemote saludoBean = (SaludoBeanRemote) InitialContext
-				.doLookup("/PruebaEJB/SaludoBean!Services.SaludoBeanRemote");
+	public static void main(String[] args){
+		SaludoBeanRemote saludoBean;
+		try {
+			saludoBean = (SaludoBeanRemote) InitialContext
+					.doLookup("/PruebaEJB/SaludoBean!Services.SaludoBeanRemote");
+			System.out.println(saludoBean.saludo("TLPI-2020-02"));
+			System.out.println("Excelente!");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		System.out.println(saludoBean.saludo("TLPI-2020-02"));
-		System.out.println("Excelente!");
+		
 
 	}
 }
